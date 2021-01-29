a5ecee0aa734b808b6063de0c5275ecc457c9d4f
lfli <haojierre@gmail.com>
2021-01-29 08:53:49 +0800

    基本框架

---
+++ src/components/Header/MyProject.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>个人项目</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class MyProject extends Vue {}
+</script>
+
+<style scoped>
+div {
+  background-color: #065fd4;
+  height: 100%;
+  width: 10vw;
+  display: flex;
+  justify-content: center;
+}
+div span {
+  color: white;
+  align-self: center;
+  font-family: cursive;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/Sidebar/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sidebar-box"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class Sidebar extends Vue {}
+</script>
+
+<style scoped>
+.sidebar-box {
+    background-color: cornflowerblue;
+}
+</style>
