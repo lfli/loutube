c18d1a3c1b190a168c38555930af3594a5d7d248
lfli <haojierre@gmail.com>
2021-01-31 14:29:11 +0800

    实现 VideoShowTempOne 组件

---
+++ src/share/VideoShowTempOne.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="temp-box">
+    <div class="temp-box-layer">
+      <div class="box-video"></div>
+      <div style="height: 12px"></div>
+      <div class="box-content">
+        <img
+          width="36"
+          class="head"
+          src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1481892154,4046723754&fm=26&gp=0.jpg"
+          alt=""
+        />
+        <div class="content-right">
+          <div class="right-top">
+            <span class="text"
+              >脱力タイムズ 浜辺美波のマイブーム 現実逃避!？</span
+            >
+            <span class="iconfont icon-menu- item-setting"></span>
+          </div>
+          <div class="right-bottom">
+            <span>老高与小茉</span><br />
+            <span>123万次观看·一年前</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class VideoShowTempOne extends Vue {}
+</script>
+
+<style scoped>
+.temp-box {
+  height: 0;
+  padding-bottom: calc(61% + 94px); /** video 高宽比 0.61 */
+  position: relative;
+}
+.temp-box-layer {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.box-video {
+  flex: 1;
+  width: 100%;
+  background-color: black;
+}
+.box-content {
+  width: 100%;
+  height: 82px;
+  display: flex;
+  align-items: flex-start;
+}
+.head {
+  border-radius: 50%;
+  margin-right: 12px;
+}
+.content-right {
+  flex: 1;
+}
+.content-right span {
+  font-size: 14px;
+  font-family: Roboto, Arial, sans-serif;
+  color: #606060;
+}
+.right-top {
+  display: flex;
+  margin-bottom: 10px;
+}
+.text {
+  color: #030303;
+  flex: 1;
+}
+.item-setting {
+  font-size: 24px !important;
+  color: #909090;
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="home">
-    <div v-for="n in 9" :key="n">{{ n }} </div>
+  <div>
+    <div class="videos-box">
+      <div v-for="n in 60" :key="n">
+        <VideoShowTempOne />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
+import VideoShowTempOne from "@/share/VideoShowTempOne.vue";
 
 @Options({
   components: {
-
+    VideoShowTempOne,
   },
 })
 export default class Home extends Vue {}
 </script>
 
 <style scoped>
-.home {
+.videos-box {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 320px));
-  grid-template-rows: repeat(auto-fill, minmax(140px, 180px));
-  gap: 20px;
-  overflow-y: scroll;
-}
-.home div {
-  background-color: aqua;
-  border: 1px solid red;
-  height: 180px;
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+  gap: 40px 16px;
+  margin-top: 24px;
+  padding: 0 24px;
 }
 </style>
