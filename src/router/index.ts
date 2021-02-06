4540ed3a892269e1472ab9f550c3bbc4b736e024
lfli <haojierre@gmail.com>
2021-02-06 16:33:12 +0800

    删除多余代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -61,13 +61,4 @@ const router = createRouter({
   routes
 })
 
-// 一级路由缓存策略
-export const keepAliveInclude = [
-  'Home',
-  'PopularNow',
-  'Subscription',
-  'MediaLibrary',
-  'History'
-]
-
 export default router
