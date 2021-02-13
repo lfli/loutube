1bdc97cbfdbea76761b52927d849add51756561a
lfli <haojierre@gmail.com>
2021-02-13 16:20:10 +0800

    调整 UI

--- src/share/Comment.vue
+++ src/share/Comment.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="comment">
     <img class="head" :src="comment.user.avatarUrl" alt="" />
-    <div>
+    <div class="comment-right">
       <div>
-        <span>{{ comment.user.nickname }}</span>
+        <span class="nickname">{{ comment.user.nickname }}</span>
+      </div>
+      <div class="content-box">
+        <span class="hideText comment-content">{{ comment.content }}</span>
       </div>
       <div>
-        <span class="hideText comment-content">{{ comment.content }}</span>
+        <div class="zan-box">
+          <span class="iconfont icon-zan comment-zan"></span>
+          <span class="zan-count">{{
+            comment.likedCount !== 0 ? comment.likedCount : ""
+          }}</span>
+          <span class="iconfont icon-zan comment-cai"></span>
+        </div>
       </div>
     </div>
   </div>
@@ -33,7 +42,41 @@ export default class Comment extends Vue {
   height: 40px;
   border-radius: 50%;
 }
+.comment-right {
+  margin-left: 18px;
+}
+.nickname {
+  font-size: 13px;
+  color: #030303;
+}
+.content-box {
+  margin-top: 3px;
+}
 .comment-content {
   -webkit-line-clamp: 2;
+  font-size: 14px;
+  color: #030303;
+}
+.zan-box {
+  padding: 14px 0 8px 0;
+  display: flex;
+  align-items: center;
+}
+.comment-zan {
+  font-size: 18px;
+  color: #909090;
+}
+.zan-count {
+  font-size: 13px;
+  color: #606060;
+  margin: 0 16px 0 8px;
+}
+.comment-cai {
+  font-size: 18px;
+  color: #909090;
+}
+.comment-cai:before {
+  display: inline-block;
+  transform: rotate(180deg);
 }
 </style>
