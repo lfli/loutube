1762056a0e8131ccf47686bf6b90a2353606a93f
lfli <haojierre@gmail.com>
2021-02-15 11:24:48 +0800

    删除 store WatchMv

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from 'vuex';
-import WatchMv from './modules/WatchMv';
 import HistoryMv from './modules/HistoryMv';
 
 export default createStore({
   modules: {
-    WatchMv,
     HistoryMv
   }
 })
