942e8136e92dbee42615a5b772ad5c9130aca3ab
lfli <haojierre@gmail.com>
2021-02-17 13:23:09 +0800

    实现 MediaLibrary 页面

--- src/views/MediaLibrary/index.vue
+++ src/views/MediaLibrary/index.vue
@@ -1,20 +1,72 @@
 <template>
-  <div class="home">
-    媒体库
+  <div>
+    <div class="media-library">
+      <div class="media-library-title" v-if="likedMvList.length === 0">
+        <span>顶过的视频</span>
+      </div>
+
+      <div class="videos-box" v-if="likedMvList.length > 0">
+        <div class="media-library-title">
+          <span>顶过的视频</span>
+        </div>
+
+        <template v-for="mv of likedMvList" :key="mv.id">
+          <VideoShowTempOne :mv="mv" @click="goWatch(mv)" />
+        </template>
+      </div>
+
+      <div class="media-library-none" v-if="likedMvList.length === 0">
+        <span>此列表中没有任何视频。</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
+import { mapState } from "vuex";
+import VideoShowTempOne from "@/share/VideoShowTempOne.vue";
+import router from "@/router";
+import { IMv } from "@/typing";
 
 @Options({
   components: {
-
+    VideoShowTempOne,
+  },
+  computed: {
+    ...mapState({
+      likedMvList: (state: any) => state.LikedMv.likedMvList,
+    }),
   },
 })
-export default class MediaLibrary extends Vue {}
+export default class MediaLibrary extends Vue {
+  goWatch(mv: IMv) {
+    router.push({ path: `/watch/${mv.id}` });
+  }
+}
 </script>
 
 <style scoped>
-
+.media-library {
+  padding: 22px 5vw;
+}
+.videos-box {
+  display: grid;
+  grid-template-columns: [c1] repeat(auto-fill, minmax(210px, 1fr)) [c2];
+  justify-content: space-between;
+  gap: 26px 4px;
+  padding-bottom: 26px;
+  margin-bottom: 26px;
+}
+.media-library-title {
+  font-size: 16px;
+  color: #030303;
+  grid-column: c1 / c2;
+}
+.media-library-none {
+  font-size: 14px;
+  color: #030303;
+  text-align: center;
+  margin-top: 10vh;
+}
 </style>
