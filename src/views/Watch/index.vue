fe810f43b5122d336166457251768cb1f681aff7
lfli <haojierre@gmail.com>
2021-02-20 09:47:08 +0800

    调整 UI

--- src/views/Watch/index.vue
+++ src/views/Watch/index.vue
@@ -130,7 +130,7 @@
           <VideoShowTempTwo :mv="mv" @click="goWatch(mv)"></VideoShowTempTwo>
           <div style="height: 8px"></div>
         </template>
-        <div class="mv-list-load-click" @click="mvListLoadingForClick()">
+        <div class="mv-list-load-click ripple" @click="mvListLoadingForClick()">
           展开
         </div>
       </div>
@@ -762,5 +762,31 @@ export default class Watch extends Vue {
   color: #030303;
   font-size: 16px;
   margin-bottom: 24px;
+}
+
+.ripple {
+  position: relative;
+  overflow: hidden;
+}
+.ripple:after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  background-image: radial-gradient(circle, #666 10%, transparent 10.01%);
+  background-repeat: no-repeat;
+  background-position: 50%;
+  transform: scale(10, 10);
+  opacity: 0;
+  transition: transform 0.3s, opacity 0.5s;
+}
+.ripple:active:after {
+  transform: scale(0, 0);
+  opacity: 0.3;
+  transition: 0s;
 }
 </style>
