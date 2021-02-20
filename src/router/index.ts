b16ac06ba22af2672eabbb9aaa5ef72b27951890
lfli <haojierre@gmail.com>
2021-02-20 13:52:55 +0800

    修改 base

--- src/router/index.ts
+++ src/router/index.ts
@@ -76,7 +76,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('loutube'),
   routes
 })
 
