7b4fc9117b58eca5944992d3ea7316bdcbfbee6f
李林锋 <lilinfeng@lilinfengdeiMac.ctc>
2021-11-29 21:45:15 +0800

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [{
+    name: 'LouTube',
+    script: './server/server.js',
+    env: {
+      SSR: 0
+    },
+    env_production: {
+      SSR: 1
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '47.99.122.43',
+      ref  : 'origin/main',
+      repo : 'git@github.com:lfli/loutube.git',
+      path : '/root/projects/production',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
